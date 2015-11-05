7603404edce455763cd3ab86d59f8cbf1f4bdaca
Martin Salazard <martin.der.aze@gmail.com>
2015-11-05 23:47:25 +0100

    adding log activiy

---
+++ app/src/main/java/net/tetrakoopa/canardhttpd/LogActivity.java
@@ -0,0 +1,34 @@
+package net.tetrakoopa.canardhttpd;
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+
+import net.tetrakoopa.mdu.android.util.ResourcesUtil;
+
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+public class LogActivity extends Activity {
+
+	public final static String MANIFEST_ACTIVITY = CanardHTTPDActivity.MANIFEST_PACKAGE+".LogActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_log);
+		final ActionBar actionBar = getActionBar();
+		if (actionBar!=null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+
+		//View mainView = findViewById(R.id.main);
+
+	}
+
+	private String message(int id) {
+		return ResourcesUtil.getString(this, id);
+	}
+
+}
