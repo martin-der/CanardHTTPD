cd5ee344490b92606639fc5b9bc2b3e6ea2f458b
Martin Salazard <martin.der.aze@gmail.com>
2015-11-30 23:51:35 +0100

    Add file size formater

--- mdua/src/main/java/net/tetrakoopa/mdu/util/FileUtil.java
+++ mdua/src/main/java/net/tetrakoopa/mdu/util/FileUtil.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class FileUtil {
 
@@ -36,6 +38,51 @@ public class FileUtil {
 		} finally {
 			in.close();
 		}
+	}
+
+	/**
+	 * Equivalent to <code>fileSizeAsString(size, "%1.3f %2so.")</code>
+	 */
+	public static String fileSizeAsString(long size) {
+		return fileSizeAsString(size, "%1.3f %2so.");
+	}
+
+	/**
+	 * @param format format used to display result, accept two place holders
+	 *            <table>
+	 *                <tr><th>Place Holder</th><th>Type</th><th>example</th></tr>
+	 *                <tr><td>size</td><td>Float or Double</td><td><code>%1f</code></td></tr>
+	 *                <tr><td>dimension</td><td>String</td><td><code>%2s</code></td></tr>
+	 *            </table>
+	 */
+	public static String fileSizeAsString(long size, String format) {
+		final double bytes = size;
+		final double kilobytes = (bytes / 1024);
+		if (kilobytes <= 0)
+			return String.format(format, bytes, "");
+		final double megabytes = (kilobytes / 1024);
+		if (megabytes <= 0)
+			return String.format(format, kilobytes, "k");
+		final double gigabytes = (megabytes / 1024);
+		if (gigabytes <= 0)
+			return String.format(format, megabytes, "m");
+		final double terabytes = (gigabytes / 1024);
+		if (terabytes <= 0)
+			return String.format(format, gigabytes, "g");
+		final double petabytes = (terabytes / 1024);
+		if (petabytes <= 0)
+			return String.format(format, terabytes, "t");
+		final double exabytes = (petabytes / 1024);
+		if (exabytes <= 0)
+			return String.format(format, petabytes, "p");
+		final double zettabytes = (exabytes / 1024);
+		if (zettabytes <= 0)
+			return String.format(format, exabytes, "e");
+		final double yottabytes = (zettabytes / 1024);
+		if (yottabytes <= 0)
+			return String.format(format, zettabytes, "z");
+		return String.format(format, yottabytes, "y");
+
 	}
 
 }
